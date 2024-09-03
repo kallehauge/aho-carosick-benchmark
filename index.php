e9ab1a4d413ed740fd8b145c792e8cf0d4011ae6
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2024-09-04 00:12:31 +0200

    Add an "all strategies" argument

--- index.php
+++ index.php
@@ -2,16 +2,35 @@
 
 namespace Kallehauge\AhoCorasick;
 
+use Kallehauge\AhoCorasick\Strategy\MatchingStrategyType;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $strategy   = isset( $argv[1] ) ? (string) $argv[1] : 'aho-corasick';
 $test_type  = isset( $argv[2] ) ? (string) $argv[2] : 'mobydick';
 $iterations = isset( $argv[3] ) ? (int) $argv[3] : 1;
 
-$matcher = new Matcher( $strategy );
-$reports = array();
-for ( $i = 0; $i < $iterations; $i ++ ) {
-	$reports[] = $matcher->start( KeywordsLibrary::get(), TextLibrary::get( $test_type ) );
+if ( $strategy === 'all' ) {
+	foreach ( MatchingStrategyType::cases() as $strategy_enum ) {
+		$reports = [];
+		$matcher = new Matcher( $strategy_enum->value );
+		for ( $i = 0; $i < $iterations; $i ++ ) {
+			$reports[] = $matcher->start( KeywordsLibrary::get(), TextLibrary::get( $test_type ) );
+		}
+
+		printf( "Strategy: %s\n", $strategy_enum->value );
+		Reporter::report( $reports );
+		echo "---------------------------------------\n";
+	}
+} else {
+	$reports = [];
+	$matcher = new Matcher( $strategy );
+	for ( $i = 0; $i < $iterations; $i ++ ) {
+		$reports[] = $matcher->start( KeywordsLibrary::get(), TextLibrary::get( $test_type ) );
+	}
+
+	printf( "Strategy: %s\n", $strategy );
+	Reporter::report( $reports );
+	echo "---------------------------------------\n";
 }
 
-Reporter::report( $reports );
