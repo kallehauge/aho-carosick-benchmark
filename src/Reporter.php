470fedade8ac53ffa27d7223f4f623c0f2703738
André Kallehauge <3846700+kallehauge@users.noreply.github.com>
2024-09-04 00:14:51 +0200

    Change bulk reporting to be summaries

--- src/Reporter.php
+++ src/Reporter.php
@@ -13,26 +13,31 @@ class Reporter {
 			return;
 		}
 
+		$reports_count = count( $reports );
+
 		// Accumulate the average execution time.
 		$total_execution_time = 0;
 		foreach ( $reports as $report ) {
 			$total_execution_time += $report->get_execution_time_in_milliseconds();
-			printf(
-				"Execution time: %s milliseconds\n",
-				number_format( $report->get_execution_time_in_milliseconds(), 2 )
-			);
-		}
 
-		echo "---------------------------------------\n";
+			if ( 1 === $reports_count ) {
+				printf(
+					"Execution time: %s milliseconds\n",
+					number_format( $report->get_execution_time_in_milliseconds(), 2 )
+				);
+			}
+		}
 
 		// Report memory usage for 1 iteration (in KB).
 		printf( "Memory usage: %s\n", self::format_memory_as_kb( $reports[0]->get_memory_usage() ) );
 
-		// Report average execution time (in MS).
-		printf(
-			"Average execution time: %s milliseconds\n",
-			number_format( $total_execution_time / count( $reports ), 2 )
-		);
+		if ( $reports_count > 1 ) {
+			// Report average execution time (in MS).
+			printf(
+				"Average execution time: %s milliseconds\n",
+				number_format( $total_execution_time / count( $reports ), 2 )
+			);
+		}
 	}
 
 	public static function format_memory_as_kb( $memory ) {
